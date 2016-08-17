38eebe8e9d722588e1d68f951b2cd2623b95d419
Yesi <317447721@qq.com>
2016-08-17 10:10:32 +0800

    Initial Commit

---
+++ BSBDJ1/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BSBDJ1
+//
+//  Created by Yesi on 16/8/17.
+//  Copyright © 2016年 Yesi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BSBDJ1Tests/BSBDJ1Tests.m
@@ -0,0 +1,39 @@
+//
+//  BSBDJ1Tests.m
+//  BSBDJ1Tests
+//
+//  Created by Yesi on 16/8/17.
+//  Copyright © 2016年 Yesi. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface BSBDJ1Tests : XCTestCase
+
+@end
+
+@implementation BSBDJ1Tests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
